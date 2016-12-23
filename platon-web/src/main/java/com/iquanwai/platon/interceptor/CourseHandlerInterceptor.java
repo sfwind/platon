341bec5e69f4d1876b146fd2597ab7e94a351f8c
Ding Zhi Jun <dzjnamegoal@hotmail.com>
2016-12-23 21:00:50 +0800

    debug open

--- platon-web/src/main/java/com/iquanwai/platon/interceptor/CourseHandlerInterceptor.java
+++ platon-web/src/main/java/com/iquanwai/platon/interceptor/CourseHandlerInterceptor.java
@@ -18,6 +18,10 @@ public class CourseHandlerInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if(!ConfigUtils.isDebug()) {
+            // 前端debug开启时,不校验
+            if(request.getParameter("debug")!=null && ConfigUtils.isFrontDebug()){
+                return true;
+            }
             String value = CookieUtils.getCookie(request, OAuthService.ACCESS_TOKEN_COOKIE_NAME);
             //没有access_token,跳转去授权
             if (StringUtils.isEmpty(value)) {
