d26f740990c76b738e5cc816dbeac1b85b8f554a
Ding Zhi Jun <dzjnamegoal@hotmail.com>
2016-12-29 22:29:23 +0800

    practice

--- platon-biz/src/main/java/com/iquanwai/platon/biz/po/ApplicationPractice.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/po/ApplicationPractice.java
@@ -8,8 +8,8 @@ import lombok.Data;
 @Data
 public class ApplicationPractice {
     private int id;
+    private String topic; //任务标题
     private String description; // 题干
-    private String pic; // 图片链接
     private Integer knowledgeId; //知识点id
     private Integer sceneId; //子场景id
     private Integer difficulty; //难易度（1-容易，2-普通，3-困难）
