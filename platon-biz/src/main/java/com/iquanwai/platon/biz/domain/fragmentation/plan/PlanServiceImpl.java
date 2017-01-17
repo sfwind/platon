d5f42f7668240d06b9939df1274964d49e331cb0
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-01-17 11:32:17 +0800

    fix

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/PlanServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/PlanServiceImpl.java
@@ -216,7 +216,13 @@ public class PlanServiceImpl implements PlanService {
         Assert.notNull(improvementPlan, "训练计划不能为空");
         Assert.notNull(practicePlans, "练习计划不能为空");
         List<PracticePlan> runningPractice = Lists.newArrayList();
-        //如果有解锁钥匙,找到第一组未完成的热身练习,如果没有解锁钥匙,找到最后一组已解锁的练习
+        //如果专题训练未完成,直接返回第一组练习
+        // TODO:不支持多组专题训练
+        PracticePlan challengePractice = practicePlanDao.loadChallengePractice(improvementPlan.getId());
+        if(challengePractice.getStatus()==0){
+            return pickPracticeBySeries(improvementPlan, 1);
+        }
+        //如果有解锁钥匙,找到第一组未完成的练习,如果没有解锁钥匙,找到最后一组已解锁的练习
         //未完成的练习
         List<PracticePlan> incompletePractice = getFirstImcompletePractice(practicePlans);
 
@@ -239,8 +245,7 @@ public class PlanServiceImpl implements PlanService {
             //第一天增加专题训练,其余时间不显示专题训练
             PracticePlan plan = runningPractice.get(0);
             if(plan.getSeries()==1) {
-                runningPractice.addAll(practicePlans.stream().filter(
-                        practicePlan -> practicePlan.getType() == PracticePlan.CHALLENGE).collect(Collectors.toList()));
+                runningPractice.add(challengePractice);
             }
         }
 
