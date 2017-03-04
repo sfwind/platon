b77736df490ff36b1340591de480149bc924aea4
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-03-04 18:07:56 +0800

    update plan

--- platon-biz/src/main/java/com/iquanwai/platon/biz/po/ImprovementPlan.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/po/ImprovementPlan.java
@@ -15,8 +15,8 @@ public class ImprovementPlan {
     private String openid; //openid
     private Integer problemId; //问题id
     private Date startDate; //开始日期
-    private Date endDate; //结束日期
-    private Date closeDate; //课程关闭时间
+    private Date endDate; //结束日期(当日开始复习)
+    private Date closeDate; //课程关闭时间（课程关闭日期）
     private Integer status; //执行状态（1-正在进行, 2-已结束, 3-已过期）
     private Integer point; //积分
     private Integer warmupComplete; //热身训练完成数量
