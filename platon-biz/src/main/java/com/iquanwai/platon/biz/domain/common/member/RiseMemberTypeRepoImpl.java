3a7459668ce46d449f626a518e3b77bf9f2e7271
nethunder <17089113140@163.com>
2017-04-07 10:35:03 +0800

    fix

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/member/RiseMemberTypeRepoImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/member/RiseMemberTypeRepoImpl.java
@@ -3,6 +3,8 @@ package com.iquanwai.platon.biz.domain.common.member;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.iquanwai.platon.biz.po.common.MemberType;
+import com.iquanwai.platon.biz.util.ConfigUtils;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -18,9 +20,16 @@ public class RiseMemberTypeRepoImpl implements RiseMemberTypeRepo {
 
     @PostConstruct
     public void init(){
-        memberTypes.put(1, new MemberType(1, 498D, "线上半年498"));
-        memberTypes.put(2, new MemberType(2, 898D, "线上一年898"));
-        memberTypes.put(3, new MemberType(3, 1980D, "线上一年+线下+学习报告1980"));
+        List<Double> riseMemberPrice = ConfigUtils.getRiseMemberPrice();
+        if (CollectionUtils.isNotEmpty(riseMemberPrice) && CollectionUtils.size(riseMemberPrice) == 3) {
+            memberTypes.put(1, new MemberType(1, riseMemberPrice.get(0), "线上半年498"));
+            memberTypes.put(2, new MemberType(2, riseMemberPrice.get(1), "线上一年898"));
+            memberTypes.put(3, new MemberType(3, riseMemberPrice.get(2), "线上一年+线下+学习报告1980"));
+        } else {
+            memberTypes.put(1, new MemberType(1, 498D, "线上半年498"));
+            memberTypes.put(2, new MemberType(2, 898D, "线上一年898"));
+            memberTypes.put(3, new MemberType(3, 1980D, "线上一年+线下+学习报告1980"));
+        }
     }
 
     @Override
--- platon-biz/src/main/java/com/iquanwai/platon/biz/util/ConfigUtils.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/util/ConfigUtils.java
@@ -1,5 +1,6 @@
 package com.iquanwai.platon.biz.util;
 
+import com.google.common.collect.Lists;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
@@ -138,5 +139,17 @@ public class ConfigUtils {
 
 	public static String getPicturePrefix(){
 		return config.getString("qiniu.picture.prefix");
+	}
+
+	public static List<Double> getRiseMemberPrice(){
+		if (config.hasPath("rise.member.price")) {
+			return config.getDoubleList("rise.member.price");
+		} else {
+			return Lists.newArrayList();
+		}
+	}
+
+	public static Boolean isDevelopment(){
+		return config.hasPath("development") && config.getBoolean("development");
 	}
 }
