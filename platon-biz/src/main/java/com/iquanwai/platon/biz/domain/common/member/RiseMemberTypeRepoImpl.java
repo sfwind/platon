55e3cd113bd00f6cea1323e5fbcc8d247e0d4d44
nethunder <17089113140@163.com>
2017-04-07 10:48:11 +0800

    fix 打印rise会员价格

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/member/RiseMemberTypeRepoImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/member/RiseMemberTypeRepoImpl.java
@@ -5,6 +5,8 @@ import com.google.common.collect.Maps;
 import com.iquanwai.platon.biz.po.common.MemberType;
 import com.iquanwai.platon.biz.util.ConfigUtils;
 import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -17,6 +19,7 @@ import java.util.Map;
 @Service
 public class RiseMemberTypeRepoImpl implements RiseMemberTypeRepo {
     private static Map<Integer, MemberType> memberTypes = Maps.newConcurrentMap();
+    private static Logger logger = LoggerFactory.getLogger(RiseMemberTypeRepoImpl.class);
 
     @PostConstruct
     public void init(){
@@ -30,6 +33,7 @@ public class RiseMemberTypeRepoImpl implements RiseMemberTypeRepo {
             memberTypes.put(2, new MemberType(2, 898D, "线上一年898"));
             memberTypes.put(3, new MemberType(3, 1980D, "线上一年+线下+学习报告1980"));
         }
+        logger.info("rise会员价格:{}", memberTypes);
     }
 
     @Override
