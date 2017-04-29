6ec92c2ee15a9daa976da9b1e74a8cfa8b3bad2e
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-04-29 12:46:39 +0800

    update role

--- platon-biz/src/main/java/com/iquanwai/platon/biz/po/common/Role.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/po/common/Role.java
@@ -17,16 +17,16 @@ public class Role {
     //常量值需和数据库持久化数据保持一致
     public static final Integer STRANGE = 0; //陌生人
     public static final Integer STUDENT = 1; //学生
-    public static final Integer COACH_LEVEL_1 = 15; //见习教练
-    public static final Integer COACH_LEVEL_2 = 20; //教练
-    public static final Integer COACH_LEVEL_3 = 30; //高级教练
-    public static final Integer COACH_LEVEL_4 = 40; //首席教练
+    public static final Integer COACH_LEVEL_1 = 3; //见习教练
+    public static final Integer COACH_LEVEL_2 = 4; //教练
+    public static final Integer COACH_LEVEL_3 = 5; //高级教练
+    public static final Integer COACH_LEVEL_4 = 6; //首席教练
 
-    public static final Integer BIG_VIP = 9; //大V
-    public static final Integer EMPLOYEE = 10; //员工
-    public static final Integer CONTENT_OPERATION = 60; //内容运营
-    public static final Integer CONTENT_ADMIN = 100; //内容管理员
-    public static final Integer ADMIN = 1000; //管理员
+    public static final Integer BIG_VIP = 7; //大V
+    public static final Integer EMPLOYEE = 2; //员工
+    public static final Integer CONTENT_OPERATION = 8; //内容运营
+    public static final Integer CONTENT_ADMIN = 9; //内容管理员
+    public static final Integer ADMIN = 10; //管理员
 
     private static List<Integer> asstRoleLevel = Lists.newArrayList(COACH_LEVEL_1, COACH_LEVEL_2,
             COACH_LEVEL_3, COACH_LEVEL_4, CONTENT_OPERATION, CONTENT_ADMIN);
