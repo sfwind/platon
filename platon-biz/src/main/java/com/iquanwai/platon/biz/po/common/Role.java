312f1291472c0b88dbb3275160b8a876d99362b2
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-05-27 15:14:18 +0800

    asst role check fix

--- platon-biz/src/main/java/com/iquanwai/platon/biz/po/common/Role.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/po/common/Role.java
@@ -16,23 +16,23 @@ public class Role {
 
     //常量值需和数据库持久化数据保持一致
     public static final Integer STRANGE = 0; //陌生人
-    public static final Integer STUDENT = 1; //学生
+    public static final Integer STUDENT = 1; //普通用户
+    public static final Integer EMPLOYEE = 2; //工作人员
     public static final Integer COACH_LEVEL_1 = 3; //见习教练
     public static final Integer COACH_LEVEL_2 = 4; //教练
     public static final Integer COACH_LEVEL_3 = 5; //高级教练
     public static final Integer COACH_LEVEL_4 = 6; //首席教练
-
     public static final Integer BIG_VIP = 7; //大V
-    public static final Integer EMPLOYEE = 2; //员工
-    public static final Integer CONTENT_OPERATION = 8; //内容运营
-    public static final Integer CONTENT_ADMIN = 9; //内容管理员
-    public static final Integer ADMIN = 10; //管理员
+    public static final Integer CONTENT_ADMIN = 8; //内容管理员
+    public static final Integer ADMIN = 9; //管理员
+    public static final Integer CONTENT_OPERATION = 10; //内容运营
 
     private static List<Integer> asstRoleLevel = Lists.newArrayList(COACH_LEVEL_1, COACH_LEVEL_2,
             COACH_LEVEL_3, COACH_LEVEL_4, CONTENT_OPERATION, CONTENT_ADMIN);
 
     public static Role stranger(){
         Role stranger = new Role();
+        stranger.setId(STRANGE);
         stranger.setName("陌生人");
         stranger.setLevel(STRANGE);
         return stranger;
@@ -40,6 +40,7 @@ public class Role {
 
     public static Role student(){
         Role stranger = new Role();
+        stranger.setId(STUDENT);
         stranger.setName("学生");
         stranger.setLevel(STUDENT);
         return stranger;
@@ -48,4 +49,5 @@ public class Role {
     public static boolean isAsst(int roleLevel){
         return asstRoleLevel.contains(roleLevel);
     }
+
 }
