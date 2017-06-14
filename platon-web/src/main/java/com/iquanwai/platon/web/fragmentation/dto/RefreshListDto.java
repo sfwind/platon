009cd6a52fa94df186774e4c7914129f33f97e46
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-06-14 18:41:18 +0800

    fix

--- platon-web/src/main/java/com/iquanwai/platon/web/fragmentation/dto/RefreshListDto.java
+++ platon-web/src/main/java/com/iquanwai/platon/web/fragmentation/dto/RefreshListDto.java
@@ -12,5 +12,5 @@ public class RefreshListDto<T> {
     private List<T> list;
     private List<T> highlightList;
     private boolean end;
-    private boolean isModifiedAfterFeedback; //是否在教练反馈后修改文章
+    private Boolean isModifiedAfterFeedback; //是否在教练反馈后修改文章
 }
