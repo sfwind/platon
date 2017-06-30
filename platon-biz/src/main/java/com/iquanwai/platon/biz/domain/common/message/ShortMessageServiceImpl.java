9d8e731452aa6a1452eba11966f8643b890f99f1
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-06-30 18:48:17 +0800

    body

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/message/ShortMessageServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/message/ShortMessageServiceImpl.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.iquanwai.platon.biz.dao.RedisUtil;
 import com.iquanwai.platon.biz.util.ConfigUtils;
 import com.iquanwai.platon.biz.util.RestfulHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,11 +25,14 @@ public class ShortMessageServiceImpl implements ShortMessageService {
 
     private static final int SUCCESS = 200;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public boolean sendShortMessage(SMSDto smsDto) {
         String shortMessageUrl = ConfigUtils.sendShortMessageUrl();
         Gson gson = new Gson();
         String body = restfulHelper.post(shortMessageUrl, gson.toJson(smsDto));
+        logger.info("{}", body);
         ResultDto resultDto = gson.fromJson(body, ResultDto.class);
         if (resultDto != null) {
             return resultDto.getCode() == SUCCESS;
