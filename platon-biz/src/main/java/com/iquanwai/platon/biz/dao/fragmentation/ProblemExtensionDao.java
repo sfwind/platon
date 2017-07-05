045236b91fdae7040e8a6add3c3d26506563b5a8
xfduan <1047210087@qq.com>
2017-07-05 13:57:43 +0800

    1

--- platon-biz/src/main/java/com/iquanwai/platon/biz/dao/fragmentation/ProblemExtensionDao.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/dao/fragmentation/ProblemExtensionDao.java
@@ -6,12 +6,14 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
 
 /**
  * Created by xfduan on 2017/7/5.
  */
+@Repository
 public class ProblemExtensionDao extends PracticeDBUtil {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
