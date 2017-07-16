9bc3032b6e5752eadfbe4fe07c9bb01a065c0897
xfduan <1047210087@qq.com>
2017-07-16 11:56:36 +0800

    fix receiver

--- platon-web/src/main/java/com/iquanwai/platon/mq/SubscribeReceiver.java
+++ platon-web/src/main/java/com/iquanwai/platon/mq/SubscribeReceiver.java
@@ -1,10 +1,14 @@
 package com.iquanwai.platon.mq;
 
+import com.alibaba.fastjson.JSONObject;
+import com.iquanwai.platon.biz.domain.fragmentation.operation.OperationService;
 import com.iquanwai.platon.biz.util.ConfigUtils;
 import com.iquanwai.platon.biz.util.rabbitmq.RabbitMQReceiver;
 import com.rabbitmq.client.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -12,18 +16,22 @@ import java.io.IOException;
 /**
  * Created by xfduan on 2017/7/14.
  */
-public class OperationReceiver {
+@Service
+public class SubscribeReceiver {
 
-    public static final String TOPIC = "operation";
+    public static final String TOPIC = "subscribe_quanwai";
 
     private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private OperationService operationService;
 
     @PostConstruct
     public void init() {
         RabbitMQReceiver receiver = new RabbitMQReceiver();
-        receiver.init(null, OperationReceiver.TOPIC, ConfigUtils.getRabbitMQIp(), ConfigUtils.getRabbitMQPort());
+        receiver.init(null, SubscribeReceiver.TOPIC, ConfigUtils.getRabbitMQIp(), ConfigUtils.getRabbitMQPort());
         Channel channel = receiver.getChannel();
-        logger.info("通道建立：" + OperationReceiver.TOPIC);
+        logger.info("通道建立：" + SubscribeReceiver.TOPIC);
         Consumer consumer = getConsumer(channel);
         receiver.listen(consumer);
     }
@@ -35,10 +43,12 @@ public class OperationReceiver {
                                        AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body);
                 logger.info("receiver message {}", message);
-                switch (message) {
-                    case "record":
-
-                }
+                JSONObject json = JSONObject.parseObject(message);
+                String scene = json.get("scene").toString();
+                logger.info("scene:", scene);
+                String openId = json.get("openId").toString();
+                logger.info("openId:", openId);
+                operationService.recordPromotionLevel(openId, scene);
             }
         };
     }
