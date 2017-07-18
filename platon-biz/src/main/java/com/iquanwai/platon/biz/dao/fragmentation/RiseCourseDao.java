77694b93b7ffe0ae38744fa5fe2240116c3e8150
nethunder <17089113140@163.com>
2017-07-18 17:19:03 +0800

    first update

--- platon-biz/src/main/java/com/iquanwai/platon/biz/dao/fragmentation/RiseCourseDao.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/dao/fragmentation/RiseCourseDao.java
@@ -5,7 +5,6 @@ import com.iquanwai.platon.biz.po.RiseCourseOrder;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
-import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -18,20 +17,6 @@ import java.sql.SQLException;
 @Repository
 public class RiseCourseDao extends DBUtil {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    public int insert(RiseCourseOrder riseCourseOrder) {
-        QueryRunner runner = new QueryRunner(getDataSource());
-        String sql = "insert into RiseCourseOrder(ProfileId, Openid, ProblemId, OrderId) " +
-                " VALUES (?, ?, ?, ?)";
-        try {
-            Long insertRs = runner.insert(sql, new ScalarHandler<>(),
-                    riseCourseOrder.getProfileId(), riseCourseOrder.getOpenid(), riseCourseOrder.getProblemId(), riseCourseOrder.getOrderId());
-            return insertRs.intValue();
-        } catch (SQLException e) {
-            logger.error(e.getLocalizedMessage(), e);
-        }
-        return -1;
-    }
 
     public RiseCourseOrder loadOrder(String orderId) {
         QueryRunner run = new QueryRunner(getDataSource());
