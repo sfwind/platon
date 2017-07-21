af9f0231328cf869882b7e5d341e7a202c414a32
xfduan <1047210087@qq.com>
2017-07-21 11:09:05 +0800

    fix bug; s

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/ProblemServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/ProblemServiceImpl.java
@@ -29,7 +29,6 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
