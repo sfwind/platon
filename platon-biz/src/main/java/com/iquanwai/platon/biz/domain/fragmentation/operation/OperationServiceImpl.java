852f7fde59a657016cec74ee7ac6f1d6b26004fc
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-08-03 16:22:09 +0800

    change content

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/operation/OperationServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/operation/OperationServiceImpl.java
@@ -211,13 +211,11 @@ public class OperationServiceImpl implements OperationService {
 
     @Override
     public void sendCustomerMsg(String openId) {
-
-
         Profile profile = accountService.getProfile(openId);
         //先发文字,后发图片
-        customerMessageService.sendCustomerMessage(openId, "Hi, "+profile.getNickname()+", " +
-                        "你已领取限免课程：找到本质问题，减少无效努力\n\n继续学习，请点击下方按钮“上课啦”\n\n" +
-                        "送你一张专属知识卡+30个好友免费学习名额，立即分享出去吧！",
+        customerMessageService.sendCustomerMessage(openId, "Hi，"+profile.getNickname()+"，" +
+                        "你已领取限免课程：找到本质问题，减少无效努力\n\n如需继续学习，请点击下方按钮“上课啦”\n\n" +
+                        "\uD83D\uDC47送你一张专属知识卡+30个好友免费学习名额，立即分享出去吧！",
                         Constants.WEIXIN_MESSAGE_TYPE.TEXT);
         BufferedImage bufferedImage = cardRepository.loadDefaultCardImg(profile);
         if(bufferedImage!=null){
