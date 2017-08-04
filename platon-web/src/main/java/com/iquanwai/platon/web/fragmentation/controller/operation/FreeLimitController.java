28f74b013fb50fa449be48605c50ab57ebd00cdc
xfduan <1047210087@qq.com>
2017-08-04 19:51:58 +0800

    1

--- platon-web/src/main/java/com/iquanwai/platon/web/fragmentation/controller/operation/FreeLimitController.java
+++ platon-web/src/main/java/com/iquanwai/platon/web/fragmentation/controller/operation/FreeLimitController.java
@@ -8,7 +8,6 @@ import com.iquanwai.platon.web.util.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
