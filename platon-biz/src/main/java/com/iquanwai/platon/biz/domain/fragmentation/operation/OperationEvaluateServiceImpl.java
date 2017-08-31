c78be87321e4fded22b5a9052848a9bcc06daf4b
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-08-31 19:16:08 +0800

    update

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/operation/OperationEvaluateServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/operation/OperationEvaluateServiceImpl.java
@@ -76,8 +76,8 @@ public class OperationEvaluateServiceImpl implements OperationEvaluateService {
     private static Map<Integer, String> freeSuggestionMap = Maps.newHashMap(); // 会员测评建议
     private static Map<Integer, String> resultTextMap = Maps.newHashMap(); // 发送测评结果
 
-    private static final String FREE_GET_TEXT = "【免费领取】\n\n分享下方你的专属海报，邀请3\n" +
-            "人扫码测试，即可免费领取原价99带你职场开挂的洞察力课程";
+    private static final String FREE_GET_TEXT = "【免费领取】\n\n分享下方你的专属海报,邀请3\n" +
+            "人扫码测试,即可免费领取原价99,带你职场开挂的洞察力课程";
 
     private final static String TEMP_IMAGE_PATH = "/data/static/images/";
 
