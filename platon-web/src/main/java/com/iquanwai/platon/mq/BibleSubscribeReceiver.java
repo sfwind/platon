1a1a1f0983d3ffeaff99bad1a6762ec6f83ffcc2
nethunder <17089113140@163.com>
2017-09-11 15:03:09 +0800

    update

--- platon-web/src/main/java/com/iquanwai/platon/mq/BibleSubscribeReceiver.java
+++ platon-web/src/main/java/com/iquanwai/platon/mq/BibleSubscribeReceiver.java
@@ -4,9 +4,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.iquanwai.platon.biz.dao.fragmentation.PromotionActivityDao;
 import com.iquanwai.platon.biz.dao.fragmentation.PromotionLevelDao;
 import com.iquanwai.platon.biz.domain.weixin.account.AccountService;
+import com.iquanwai.platon.biz.domain.weixin.customer.CustomerMessageService;
 import com.iquanwai.platon.biz.po.PromotionActivity;
 import com.iquanwai.platon.biz.po.PromotionLevel;
 import com.iquanwai.platon.biz.po.common.Profile;
+import com.iquanwai.platon.biz.util.Constants;
 import com.iquanwai.platon.biz.util.PromotionConstants;
 import com.iquanwai.platon.biz.util.rabbitmq.RabbitMQFactory;
 import org.apache.commons.lang3.StringUtils;
@@ -33,6 +35,8 @@ public class BibleSubscribeReceiver {
     private PromotionActivityDao promotionActivityDao;
     @Autowired
     private AccountService accountService;
+    @Autowired
+    private CustomerMessageService customerMessageService;
 
     private static final String TOPIC = "subscribe_quanwai";
     private static final String QUEUE = "BibleEvent_Queue";
@@ -77,6 +81,18 @@ public class BibleSubscribeReceiver {
             promotionActivity.setActivity(PromotionConstants.Activities.Bible);
             promotionActivity.setAction(PromotionConstants.BibleAction.ScanCode);
             promotionActivityDao.insertPromotionActivity(promotionActivity);
+
+            customerMessageService.sendCustomerMessage(profile.getOpenid(),
+                    "迎来到圈外同学！点击下方按钮【学札】，体验终身学习管理工具，和知识焦虑说拜拜~\n" +
+                    "\n" +
+                    "------------------\n" +
+                    "关于圈外同学：\n" +
+                    "\n" +
+                    "这个奇妙的号已经成功帮助了很多职场人拿到梦寐以求的offer，搞定了不可能的项目，实现了薪资3倍增长……\n" +
+                    "\n" +
+                    "这里除了有帮你在职场升级打怪，体验开挂式人生的课程，更有BAT、世界500强、硅谷创投、连续创业的同学与你一起交换资源，还有每周不定期发布的各领域大咖直播、圈圈密切互动、免费能力测评等福利…… \n" +
+                    "\n" +
+                    "希望你和圈外同学一起刻意练习、高速成长，成为“优秀到不可被取代的职场人“", Constants.WEIXIN_MESSAGE_TYPE.TEXT);
         });
 
     }
