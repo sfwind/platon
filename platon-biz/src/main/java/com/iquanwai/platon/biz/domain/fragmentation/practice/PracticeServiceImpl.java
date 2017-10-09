f22171acff8379ca3a0a95c79ba7d9affd7d2edc
xfduan <1047210087@qq.com>
2017-10-09 16:35:48 +0800

    fix

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/practice/PracticeServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/practice/PracticeServiceImpl.java
@@ -728,10 +728,13 @@ public class PracticeServiceImpl implements PracticeService {
 
     @Override
     public void initCommentEvaluation(Integer submitId, Integer commentId) {
-        // 对于一道应用题，只有一次评价
-        List<Comment> comments = commentDao.loadCommentsByProfileId(submitId, commentId);
-        if (comments.size() == 0) {
-            commentEvaluationDao.initCommentEvaluation(submitId, commentId);
+        Comment comment = commentDao.load(Comment.class, commentId);
+        if (comment != null && comment.getCommentProfileId() != null) {
+            // 对于一道应用题，只有一次评价
+            List<Comment> comments = commentDao.loadCommentsByProfileId(submitId, comment.getCommentProfileId());
+            if (comments.size() == 0) {
+                commentEvaluationDao.initCommentEvaluation(submitId, commentId);
+            }
         }
     }
 
