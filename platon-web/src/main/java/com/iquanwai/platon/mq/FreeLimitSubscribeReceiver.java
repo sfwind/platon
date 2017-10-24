96b1eb24e2ff64b56b1e6ae097713e9325bc75cf
nethunder <17089113140@163.com>
2017-10-24 12:20:02 +0800

    update

--- platon-web/src/main/java/com/iquanwai/platon/mq/FreeLimitSubscribeReceiver.java
+++ platon-web/src/main/java/com/iquanwai/platon/mq/FreeLimitSubscribeReceiver.java
@@ -6,7 +6,6 @@ import com.iquanwai.platon.biz.domain.fragmentation.operation.OperationEvaluateS
 import com.iquanwai.platon.biz.domain.fragmentation.operation.OperationFreeLimitService;
 import com.iquanwai.platon.biz.domain.weixin.account.AccountService;
 import com.iquanwai.platon.biz.domain.weixin.customer.CustomerMessageService;
-import com.iquanwai.platon.biz.po.common.Profile;
 import com.iquanwai.platon.biz.util.ConfigUtils;
 import com.iquanwai.platon.biz.util.Constants;
 import com.iquanwai.platon.biz.util.rabbitmq.RabbitMQFactory;
@@ -48,6 +47,15 @@ public class FreeLimitSubscribeReceiver {
         });
     }
 
+
+
+    private void sendScanMsg(String openId) {
+        String message = "不好，来迟了一步，洞察力测评都关闭了。\n\n" +
+                "然而，课程是开放滴，和小伙伴们一起升级职场洞察力？戳这里：\n\n" +
+                "\uD83D\uDC49<a href='" + ConfigUtils.domainName() + "/static/audition/success?choose=true'>点击领取试听小课</a>";
+        customerMessageService.sendCustomerMessage(openId, message, Constants.WEIXIN_MESSAGE_TYPE.TEXT);
+    }
+
     private void activeAction(String message) {
         logger.info("receiver message {}", message);
         JSONObject json = JSONObject.parseObject(message);
@@ -65,34 +73,7 @@ public class FreeLimitSubscribeReceiver {
             // 只记录限免小课活动
             if (sceneParams.length == 3) {
                 operationFreeLimitService.recordPromotionLevel(openId, scene);
-                // TODO 测评活动结束后删除
-                Profile profile = accountService.getProfile(openId);
-                operationEvaluateService.recordScan(profile.getId(), sceneParams[1]);
-
-                String sendMsg;
-                if (Integer.parseInt(sceneParams[2]) == ConfigUtils.getTrialProblemId()) {
-                    sendMsg = "欢迎来到【圈外职场研究所】\n\n" +
-                            "职场中有一种能力，能让人：\n\n" +
-                            "- 不用加班，也能升职加薪\n" +
-                            "- 秒懂他人心思、人缘爆表\n" +
-                            "- 提案一次通关、从不修改\n\n" +
-                            "你是否也具有这种能力?\n\n" +
-                            "\uD83D\uDC49<a href='" + ConfigUtils.domainName() + "/rise/static/eva/start'>点击开始职场洞察力检测</a>";
-                } else {
-                    // 非限免
-                    sendMsg = "欢迎来到圈外，你刚才扫码的课程在这里，点击查看：\n" +
-                            "\n" +
-                            "<a href='" + ConfigUtils.adapterDomainName() +
-                            "/rise/static/plan/view?id=" +
-                            sceneParams[2] +
-                            "'>『" + cacheService.getProblem(Integer.parseInt(sceneParams[2])).getProblem() + "』</a>\n" +
-                            "\n" +
-                            "------------\n" +
-                            "P. S. 你是高洞察力的职场人吗？\n\n" +
-                            "<a href='" + ConfigUtils.adapterDomainName() +
-                            "/rise/static/eva/start'>『点击开始测试』</a>";
-                }
-                customerMessageService.sendCustomerMessage(openId, sendMsg, Constants.WEIXIN_MESSAGE_TYPE.TEXT);
+                sendScanMsg(openId);
             }
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
