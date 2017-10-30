bedde326ea058a1e663c54be14399077625e0aef
xfduan <1047210087@qq.com>
2017-10-30 22:26:44 +0800

    auto optimize

--- platon-web/src/main/java/com/iquanwai/platon/web/personal/CustomerController.java
+++ platon-web/src/main/java/com/iquanwai/platon/web/personal/CustomerController.java
@@ -38,13 +38,6 @@ import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
