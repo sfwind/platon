e2dd81c481c070b3b762f2b45c47f69c518a5f93
xfduan <1047210087@qq.com>
2017-11-04 12:00:19 +0800

    add column

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/BusinessPlanService.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/BusinessPlanService.java
@@ -3,6 +3,7 @@ package com.iquanwai.platon.biz.domain.fragmentation.plan;
 import com.iquanwai.platon.biz.po.CourseSchedule;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by justin on 2017/11/3.
@@ -19,4 +20,17 @@ public interface BusinessPlanService {
      * @param profileId 用户id
      * */
     List<CourseSchedule> getOpeningPlan(Integer profileId);
+
+    /**
+     * 获取个人与月份相关的学习计划安排
+     * @param profileId 个人 id
+     */
+    Map<Integer, List<CourseSchedule>> getPersonalCourseSchedule(Integer profileId);
+
+    /**
+     * 查看某门小课的计划能否更改
+     * @param profileId 用户 id
+     * @param problemId 小课 id
+     */
+    boolean checkProblemModifyAccess(Integer profileId, Integer problemId);
 }
