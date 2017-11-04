f470fd4b70ca28e57c8eb4b150f0c3fb7d30c84f
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-11-04 10:30:13 +0800

    init

---
+++ platon-web/src/main/java/com/iquanwai/platon/web/fragmentation/controller/ScheduleController.java
@@ -0,0 +1,14 @@
+package com.iquanwai.platon.web.fragmentation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by justin on 2017/11/4.
+ */
+@RestController
+@RequestMapping("/rise/schedule")
+public class ScheduleController {
+
+
+}
