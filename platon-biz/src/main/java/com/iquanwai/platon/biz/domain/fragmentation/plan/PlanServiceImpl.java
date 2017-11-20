552078ebdf2941ee1fd618755f00d6de5a1b9f31
xfduan <1047210087@qq.com>
2017-11-20 09:40:33 +0800

    remove main

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/PlanServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/PlanServiceImpl.java
@@ -924,8 +924,4 @@ public class PlanServiceImpl implements PlanService {
         return classIds.get(0);
     }
 
-    public static void main(String[] args) {
-        System.out.println(TimeUnit.DAYS.toSeconds(30));
-    }
-
 }
