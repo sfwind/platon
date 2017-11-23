156c392e1e1a3d896ebe61895a68e1da2d37ad41
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-11-23 15:01:56 +0800

    sale share

--- platon-web/src/main/java/com/iquanwai/platon/mq/AuditionSignupReceiver.java
+++ platon-web/src/main/java/com/iquanwai/platon/mq/AuditionSignupReceiver.java
@@ -1,23 +1,14 @@
 package com.iquanwai.platon.mq;
 
 import com.alibaba.fastjson.JSONObject;
-import com.iquanwai.platon.biz.dao.fragmentation.PromotionActivityDao;
-import com.iquanwai.platon.biz.dao.fragmentation.PromotionLevelDao;
-import com.iquanwai.platon.biz.domain.weixin.account.AccountService;
 import com.iquanwai.platon.biz.domain.weixin.customer.CustomerMessageService;
-import com.iquanwai.platon.biz.po.PromotionActivity;
-import com.iquanwai.platon.biz.po.PromotionLevel;
-import com.iquanwai.platon.biz.po.common.Profile;
 import com.iquanwai.platon.biz.util.ConfigUtils;
 import com.iquanwai.platon.biz.util.Constants;
-import com.iquanwai.platon.biz.util.PromotionConstants;
 import com.iquanwai.platon.biz.util.rabbitmq.RabbitMQFactory;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 import javax.annotation.PostConstruct;
 
@@ -33,8 +24,10 @@ public class AuditionSignupReceiver {
     @Autowired
     private CustomerMessageService customerMessageService;
 
-    private static final String TOPIC = "subscribe_quanwai";
+    private static final String TOPIC = "audition_signup";
     private static final String QUEUE = "audition_signup_queue";
+
+    private static final String AUDITION_SUCCESS = "/pay/static/audition/success";
 
     @PostConstruct
     public void init() {
@@ -44,7 +37,7 @@ public class AuditionSignupReceiver {
             JSONObject json = JSONObject.parseObject(message.getMessage().toString());
 
             String openId = json.getString("openid");
-            customerMessageService.sendCustomerMessage(openId, ConfigUtils.domainName()+"",
+            customerMessageService.sendCustomerMessage(openId, "<a href='"+ConfigUtils.domainName()+AUDITION_SUCCESS+"'>"+"点击这里完成预约</a>",
                     Constants.WEIXIN_MESSAGE_TYPE.TEXT);
         });
 
