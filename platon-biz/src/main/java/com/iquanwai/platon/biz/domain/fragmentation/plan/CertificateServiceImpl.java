10d4a9ae2d9f5d5cd24fde58f4ccbef6b645b618
richardyangcz <richardyangcz@sina.com>
2017-12-06 14:37:32 +0600

    add log info

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/CertificateServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/CertificateServiceImpl.java
@@ -355,6 +355,8 @@ public class CertificateServiceImpl implements CertificateService {
             if (!CollectionUtils.isEmpty(improvementPlans)) {
                 profileId = improvementPlans.get(0).getProfileId();
                 problemId = improvementPlans.get(0).getProblemId();
+                logger.info("当月计划的课："+businessPlanService.getLearningProblemId(profileId).toString());
+                logger.info("现在的课："+problemId);
                 //判断是否是当前主修的problemId
                 if (businessPlanService.getLearningProblemId(profileId) == problemId) {
                     //判断是否应该发送全勤奖
