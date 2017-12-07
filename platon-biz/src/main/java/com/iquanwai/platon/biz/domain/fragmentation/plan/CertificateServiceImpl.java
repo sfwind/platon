e3bafc58de3e5cc2aba57c073bb08a56aaff0f14
richardyangcz <richardyangcz@sina.com>
2017-12-07 23:59:11 +0600

    add logs

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/CertificateServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/fragmentation/plan/CertificateServiceImpl.java
@@ -350,22 +350,23 @@ public class CertificateServiceImpl implements CertificateService {
         int problemId;
         PracticePlan plan = practicePlanDao.load(PracticePlan.class,practicePlanId);
         if (plan!=null) {
-            logger.info("planId不为空");
+            logger.info("plan不为空");
             planId = plan.getPlanId();
            ImprovementPlan improvementPlan = improvementPlanDao.load(ImprovementPlan.class,planId);
             if (improvementPlan!=null) {
                 logger.info("improvementPlan不为空");
-            if (improvementPlan!=null) {
                 profileId = improvementPlan.getProfileId();
                 problemId = improvementPlan.getProblemId();
-                logger.info("当前主修problemId:"+businessPlanService.getLearningProblemId(profileId));
-                logger.info("problemId:"+problemId);
+                logger.info("当前主修的problemId为："+businessPlanService.getLearningProblemId(profileId));
+                logger.info("您目前的problemId为："+problemId);
                 //判断是否是当前主修的problemId
                 if (businessPlanService.getLearningProblemId(profileId).equals(problemId)) {
+                    logger.info("当前课程是训练营课程");
                     //判断是否应该发送全勤奖
                     boolean isGenerate = true;
                     //获得学习内容完成情况列表
                     List<PracticePlan> practicePlans = practicePlanDao.loadPracticePlan(planId);
+                    logger.info("practiceplans的大小："+practicePlans.size());
                     if (practicePlans.size() != 0) {
                         Long unCompleteNecessaryCountLong = practicePlans.stream()
                                 .filter(practicePlan -> PracticePlan.CHALLENGE != practicePlan.getType())
