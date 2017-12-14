c96db3ef447721ba0de5e5964faa63a5e639578b
xfduan <1047210087@qq.com>
2017-12-14 19:07:24 +0800

    add comment

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/subscribe/SubscribeRouterService.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/subscribe/SubscribeRouterService.java
@@ -3,5 +3,8 @@ package com.iquanwai.platon.biz.domain.common.subscribe;
 import com.iquanwai.platon.biz.po.common.SubscribeRouterConfig;
 
 public interface SubscribeRouterService {
+    /**
+     * 根据当前 url 获取跳转路由配置
+     */
     SubscribeRouterConfig loadUnSubscribeRouterConfig(String currentPatchName);
 }
--- platon-biz/src/main/java/com/iquanwai/platon/biz/po/common/SubscribeRouterConfig.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/po/common/SubscribeRouterConfig.java
@@ -6,9 +6,9 @@ import lombok.Data;
 public class SubscribeRouterConfig {
 
     private Integer id;
-    private String url;
-    private String scene;
-    private Integer sequence;
+    private String url; // 正则 url
+    private String scene; // 场景号
+    private Integer sequence; // 匹配顺序
     private Boolean del;
 
 }
