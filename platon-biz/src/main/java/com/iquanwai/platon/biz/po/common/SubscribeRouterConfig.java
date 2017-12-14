908aba39586084ff0b123ad14aba04f8b4b8f15c
xfduan <1047210087@qq.com>
2017-12-14 19:09:47 +0800

    add annotation

--- platon-biz/src/main/java/com/iquanwai/platon/biz/po/common/SubscribeRouterConfig.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/po/common/SubscribeRouterConfig.java
@@ -8,7 +8,7 @@ public class SubscribeRouterConfig {
     private Integer id;
     private String url; // 正则 url
     private String scene; // 场景号
-    private Integer sequence; // 匹配顺序
+    private Integer sequence; // 正则匹配优先级
     private Boolean del;
 
 }
