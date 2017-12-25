48d4b0e977d4ccf55f51c507dfd56be5931db7f3
nethunder <17089113140@163.com>
2017-12-25 10:47:20 +0800

    update

--- platon-biz/src/main/java/com/iquanwai/platon/biz/po/AnnualSummary.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/po/AnnualSummary.java
@@ -8,9 +8,24 @@ import java.util.Date;
 public class AnnualSummary {
     private Integer id;
     private Integer profileId;
+    /**
+     * 加入时间
+     */
     private Date joinDate;
+    /**
+     * 第几个学员
+     */
     private Integer joinOrder;
+    /**
+     * 全对次数
+     */
     private Integer allRightCount;
+    /**
+     * 总积分
+     */
     private Integer point;
+    /**
+     * 点开知识点数量
+     */
     private Integer knowledges;
 }
