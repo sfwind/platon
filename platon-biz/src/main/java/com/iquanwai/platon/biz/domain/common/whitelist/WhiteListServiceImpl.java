c96ffd6731207835b646cecae58e6c5438e5d830
richardyangcz <richardyangcz@sina.com>
2018-01-05 14:59:34 +0600

    modify date

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/whitelist/WhiteListServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/common/whitelist/WhiteListServiceImpl.java
@@ -173,9 +173,8 @@ public class WhiteListServiceImpl implements WhiteListService {
         if (prizeCardDao.loadReceiveAnnualCard(profileId).size() == 0 && prizeCardDao.loadAnnualCardByReceiver(profileId) == null && groupPromotionDao.loadByProfileId(profileId) == null) {
             return false;
         }
-        //TODO:正式上线之前需要将日期修改成7号和14号
-        Date campOpenDate = new DateTime(2018, 1, 3, 0, 0).toDate();
-        Date campCloseDate = new DateTime(2018,1,10,0,0).toDate();
+        Date campOpenDate = new DateTime(2018, 1, 7, 0, 0).toDate();
+        Date campCloseDate = new DateTime(2018,1,14,0,0).toDate();
         //如果已经到学习时间
         if (campOpenDate.compareTo(new Date()) < 0 && campCloseDate.compareTo(new Date()) >= 0 ) {
             return true;
