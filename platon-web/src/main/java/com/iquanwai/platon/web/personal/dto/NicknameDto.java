e32197f63dd92bded249a31dacf5587341947b40
Ding Zhi Jun <dingzhijun@imdada.cn>
2018-01-09 18:55:36 +0800

    fix

---
+++ platon-web/src/main/java/com/iquanwai/platon/web/personal/dto/NicknameDto.java
@@ -0,0 +1,11 @@
+package com.iquanwai.platon.web.personal.dto;
+
+import lombok.Data;
+
+/**
+ * Created by justin on 2018/1/9.
+ */
+@Data
+public class NicknameDto {
+    private String nickname;
+}
