31a5a9c17bf29a3b7bff4c7287e87b260dc10425
xfduan <1047210087@qq.com>
2018-01-16 14:55:25 +0800

    1

--- platon-web/src/main/java/com/iquanwai/platon/web/interceptor/WeixinLoginHandlerInterceptor.java
+++ platon-web/src/main/java/com/iquanwai/platon/web/interceptor/WeixinLoginHandlerInterceptor.java
@@ -71,6 +71,7 @@ public class WeixinLoginHandlerInterceptor extends HandlerInterceptorAdapter {
     private void writeUnLoginPage(HttpServletResponse response) throws IOException {
         Map<String, Object> map = Maps.newHashMap();
         PrintWriter out = response.getWriter();
+        response.setCharacterEncoding("UTF-8");
         map.put("code", 401);
         map.put("msg", "没有登录");
         out.write(CommonUtils.mapToJson(map));
