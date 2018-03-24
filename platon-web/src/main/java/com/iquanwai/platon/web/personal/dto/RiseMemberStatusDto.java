abaae6e2ad7706fd117c348f7f7423927169388e
xfduan <1047210087@qq.com>
2018-03-24 16:09:23 +0800

    add model

--- platon-web/src/main/java/com/iquanwai/platon/web/personal/dto/RiseMemberStatusDto.java
+++ platon-web/src/main/java/com/iquanwai/platon/web/personal/dto/RiseMemberStatusDto.java
@@ -1,5 +1,6 @@
 package com.iquanwai.platon.web.personal.dto;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import lombok.Data;
  * Created by 三十文
  */
 @Data
+@ApiModel("课程页面身份状态")
 public class RiseMemberStatusDto {
 
     @ApiModelProperty("是否进入倒计时")
