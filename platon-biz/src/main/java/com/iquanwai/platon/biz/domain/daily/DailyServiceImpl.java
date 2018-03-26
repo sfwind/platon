290d745073c2fa3b3f59159fde44cfb872013f11
richardyangcz <richardyangcz@sina.com>
2018-03-26 20:51:21 +0600

    add content

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/daily/DailyServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/daily/DailyServiceImpl.java
@@ -83,6 +83,8 @@ public class DailyServiceImpl implements DailyService {
                 inputImage = ImageUtils.writeText(inputImage,280,262,"365个",font.deriveFont(34f),Color.BLACK);
                 inputImage = ImageUtils.writeText(inputImage,542,262,"12%的同学",font.deriveFont(34f),Color.BLACK);
                 inputImage = ImageUtils.writeTextCenter(inputImage,416,"----------------------每日圈语-----------------",font.deriveFont(15),Color.white);
+                inputImage = ImageUtils.writeText(inputImage,50,526,content,font.deriveFont(30f),Color.white);
+//                inputImage = ImageUtils.writeText(inputImage,460,740,"",font.deriveFont(15f),Color.white);
 
                 ImageUtils.writeToOutputStream(inputImage, "png", outputStream);
 
