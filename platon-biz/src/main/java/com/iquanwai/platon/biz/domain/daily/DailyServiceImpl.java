827fb60f818f2297a6d12c1402faff4bdb06d5b1
richardyangcz <richardyangcz@sina.com>
2018-03-27 14:39:56 +0600

    fix  content

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/daily/DailyServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/daily/DailyServiceImpl.java
@@ -95,7 +95,7 @@ public class DailyServiceImpl implements DailyService {
                 inputImage = ImageUtils.writeText(inputImage,280+30*learnKnowledge.toString().length(),292,"个",font.deriveFont(22f),grey);
                 inputImage = ImageUtils.writeText(inputImage,542,292,percent+"%",font.deriveFont(45f),Color.BLACK);
                 inputImage = ImageUtils.writeText(inputImage,542+50*percent.toString().length(),292,"的同学",font.deriveFont(22f),grey);
-                inputImage = ImageUtils.writeText(inputImage,50,566,content,font.deriveFont(60f),Color.white);
+                inputImage = ImageUtils.writeText(inputImage,50,606,content,font.deriveFont(45f),Color.white);
                 ImageUtils.writeToOutputStream(inputImage, "png", outputStream);
 
                 BASE64Encoder encoder = new BASE64Encoder();
