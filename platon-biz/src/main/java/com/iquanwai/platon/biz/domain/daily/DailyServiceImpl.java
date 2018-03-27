bda20a01e2c0af15808b4eb80b4855188768e579
richardyangcz <richardyangcz@sina.com>
2018-03-27 15:23:28 +0600

    fix content

--- platon-biz/src/main/java/com/iquanwai/platon/biz/domain/daily/DailyServiceImpl.java
+++ platon-biz/src/main/java/com/iquanwai/platon/biz/domain/daily/DailyServiceImpl.java
@@ -90,7 +90,10 @@ public class DailyServiceImpl implements DailyService {
                 if (url != null) {
                     BufferedImage contentImg = ImageUtils.copy(ImageUtils.getBufferedImageByUrl(url));
                     contentImg = ImageUtils.overlapFixImage(contentImg,titleImg,106,64,538,60);
-                    contentImg = ImageUtils.writeText(contentImg,50,174,content,font.deriveFont(45f),Color.white);
+                    String[] strs = content.split("|");
+                    for(int i = 0 ; i<strs.length;i++){
+                    contentImg = ImageUtils.writeText(contentImg,50,174+i*60,strs[i],font.deriveFont(45f),Color.white);
+                    }
                     contentImg = ImageUtils.overlapFixImage(contentImg,authorImg,460,380,64,1);
                     contentImg = ImageUtils.writeText(contentImg,540,390,author,font.deriveFont(30f),Color.WHITE);
                     inputImage = ImageUtils.overlapFixImage(inputImage, contentImg, 0, 400, 750, 504);
