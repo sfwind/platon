dddbaf2f21bbe322e79ca1bcd15f7a7dbfa2a8e5
三十文 <1047210087@qq.com>
2018-04-25 15:31:34 +0800

    remove

--- platon-web/src/main/java/com/iquanwai/platon/web/fragmentation/controller/LandingPageController.java
+++ platon-web/src/main/java/com/iquanwai/platon/web/fragmentation/controller/LandingPageController.java
@@ -143,8 +143,4 @@ public class LandingPageController {
         }
     }
 
-    public ResponseEntity<Map<String, Object>> loadSubscribeQrCode (UnionUser unionUser, ) {
-        qrCodeService.loadQrBase64("");
-    }
-
 }
